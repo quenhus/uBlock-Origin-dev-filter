b0a07f322848f7fafae8ccba8c418bf00449f7ec
Quenhus <8274288+quenhus@users.noreply.github.com>
2022-01-22 20:50:03 +0100

    feat: add header to ublock filters
    closes #13

--- src/generate.py
+++ src/generate.py
@@ -1,4 +1,3 @@
-from glob import glob
 from pathlib import Path
 
 LINE_SEP = "\n"
@@ -18,6 +17,17 @@ def to_brave(url):
 def to_startpage(url):
     return f'startpage.com##.w-gl__result:has(a[href*="{to_css_attr(url)}"])'
 
+def get_ublock_filters_header(name):
+    return f"""! Title: uBlock-Origin-dev-filter – {name}
+! Expires: 1 day
+! Description: Filters to block and remove copycat-websites from DuckDuckGo and Google. Specific to dev websites like StackOverflow or GitHub.
+! Homepage: https://github.com/quenhus/uBlock-Origin-dev-filter
+! Licence: https://github.com/quenhus/uBlock-Origin-dev-filter/blob/main/LICENSE
+!
+! GitHub issues: https://github.com/quenhus/uBlock-Origin-dev-filter/issues
+! GitHub pull requests: https://github.com/quenhus/uBlock-Origin-dev-filter/pulls
+"""
+
 def main():
     root_path = Path(__file__).parent.joinpath("../")
     dist_path = root_path.joinpath("dist")
@@ -33,11 +43,17 @@ def main():
     for f in [g_all, d_all, gd_all, b_all, sp_all]:
         f.parent.mkdir(parents=True, exist_ok=True)
 
-    with g_all.open("w") as g_all, \
-         d_all.open("w") as d_all, \
-         gd_all.open("w") as gd_all, \
-         b_all.open("w") as b_all, \
-         sp_all.open("w") as sp_all:
+    with g_all.open("w", encoding="utf8") as g_all, \
+         d_all.open("w", encoding="utf8") as d_all, \
+         gd_all.open("w", encoding="utf8") as gd_all, \
+         b_all.open("w", encoding="utf8") as b_all, \
+         sp_all.open("w", encoding="utf8") as sp_all:
+
+        g_all.write(get_ublock_filters_header("Google – All"))
+        d_all.write(get_ublock_filters_header("DuckDuckGo – All"))
+        gd_all.write(get_ublock_filters_header("Google+DuckDuckGo – All"))
+        b_all.write(get_ublock_filters_header("Brave – All"))
+        sp_all.write(get_ublock_filters_header("Startpage – All"))
 
         for file in root_path.joinpath("data").glob("*.txt"):
             filename = file.name.split(".")[0]
@@ -58,12 +74,20 @@ def main():
                         tmp.write(line)
             tmp_txt.replace(file)
 
-            with dist_path.joinpath("google", f"{filename}.txt").open("w") as g, \
-                dist_path.joinpath("duckduckgo", f"{filename}.txt").open("w") as d, \
-                dist_path.joinpath("google_duckduckgo", f"{filename}.txt").open("w") as gd, \
-                dist_path.joinpath("brave", f"{filename}.txt").open("w") as b, \
-                dist_path.joinpath("startpage", f"{filename}.txt").open("w") as sp, \
+            with dist_path.joinpath("google", f"{filename}.txt").open("w", encoding="utf8") as g, \
+                dist_path.joinpath("duckduckgo", f"{filename}.txt").open("w", encoding="utf8") as d, \
+                dist_path.joinpath("google_duckduckgo", f"{filename}.txt").open("w", encoding="utf8") as gd, \
+                dist_path.joinpath("brave", f"{filename}.txt").open("w", encoding="utf8") as b, \
+                dist_path.joinpath("startpage", f"{filename}.txt").open("w", encoding="utf8") as sp, \
                 file.open("r") as i:
+
+                filter_name = file.stem.replace("_copycats", "")
+                g.write(get_ublock_filters_header(f"Google – {filter_name}"))
+                d.write(get_ublock_filters_header(f"DuckDuckGo – {filter_name}"))
+                gd.write(get_ublock_filters_header(f"Google+DuckDuckGo – {filter_name}"))
+                b.write(get_ublock_filters_header(f"Brave – {filter_name}"))
+                sp.write(get_ublock_filters_header(f"Startpage – {filter_name}"))
+
                 for line in i:
                     if line.startswith("!") or not line.strip():
                         continue
