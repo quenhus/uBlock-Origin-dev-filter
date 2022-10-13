77703d816b979ab445e2544632c0c2b86c5cf59e
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-10-13 16:58:12 -0500

    Possible fix

--- src/generate.py
+++ src/generate.py
@@ -24,7 +24,10 @@ def to_domain_attr(url):
         .lstrip(".")
 
 def to_domain_ublock(url):
-    return f"||{format_url(url)}^$all"
+    if "/" in url:
+        return f"||{format_url(url)}$all"
+    else:
+        return f"||{format_url(url)}^$all"
 
 def to_google(url):
     return f'google.*##.g:has(a[href*="{format_url(url)}"])'
