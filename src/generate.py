32b2a541908e83e0718bea178954f1548cc971a6
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-10-13 17:08:20 -0500

    Update generate.py

--- src/generate.py
+++ src/generate.py
@@ -27,7 +27,7 @@ def to_domain_ublock(url):
     if "/" in url:
         return f"||{format_url(url)}$all"
     else:
-        return f"||{format_url(url)}^$all"
+        return f"||{format_url(to_domain_attr(url))}^$all"
 
 def to_google(url):
     return f'google.*##.g:has(a[href*="{format_url(url)}"])'
